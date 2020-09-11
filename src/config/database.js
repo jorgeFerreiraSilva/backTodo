adf95bcd1dad660a12c93efb9259c5a6ab82e107
Jorge Silva <jorgeaferreirasilva@gmail.com>
2020-09-11 09:13:27 -0300

    third commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+const { DB_HOST, DB_PORT, DB_NAME } = process.env
+module.exports = mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true })
